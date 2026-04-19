3ae0db5ddc12676cafe851a87da5dd5d5b601e76
arman10812 <armani.cabey@bayes.city.ac.uk>
2026-04-19 18:51:36 +0000

    initial marimo notebook setup

---
+++ notebook.py
@@ -0,0 +1,23 @@
+import marimo
+
+__generated_with = "0.23.1"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+
+    return
+
+
+@app.cell
+def _():
+    import pandas as pd
+    import plotly.express as px
+
+    return
+
+
+if __name__ == "__main__":
+    app.run()
